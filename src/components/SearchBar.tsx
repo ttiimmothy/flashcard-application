737b22b14ef4c317b133471b629d53efa6345b15
ttiimmothy <dinosauli2006@gmail.com>
2023-11-02 06:56:17 -0400

    Feature: add one warning message

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,12 +5,14 @@ const SearchBar: React.FC = () => {
   const [input, setInput] = useState<string>("");
   const [isLoading, setIsLoading] = useState(false);
   const [lengthError, setLengthError] = useState(false);
+  const [serverError, setServerError] = useState(false);
 
   const submitTopic = async () => {
     try {
       if (input.trim() !== "" && input.length > 10) {
         setIsLoading(true);
         await addFlashcards(input);
+        setInput("");
         setIsLoading(false);
       } else if (input.length <= 10) {
         setLengthError(true);
@@ -19,13 +21,14 @@ const SearchBar: React.FC = () => {
       }
     } catch (e) {
       setIsLoading(false);
+      setServerError(true);
       console.error(e);
       throw e;
     }
   };
 
   return (
-    <div className="flex items-start p-10 gap-8 justify-center">
+    <div className="flex items-start p-10 gap-6 justify-center">
       <div className="w-full">
         <input
           type="text"
@@ -33,13 +36,24 @@ const SearchBar: React.FC = () => {
           onChange={(e) => {
             setInput(e.target.value);
             setLengthError(false);
+            setServerError(false);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              submitTopic();
+            }
           }}
           placeholder="Enter a topic to begin generating flashcards"
           className="p-3 rounded-full border-2 border-gray-300 focus:border-violet-400 outline-none transition-colors w-full"
         />
         {lengthError && (
           <div className="text-red-500 mt-2 text-sm font-semibold w-[fit-content]">
-            Topic must be at least 10 characters long
+            Topic must be at least 10 characters long.
+          </div>
+        )}
+        {serverError && (
+          <div className="text-red-500 mt-2 text-sm font-semibold w-[fit-content]">
+            Server is broken, so the flashcards cannot be generated.
           </div>
         )}
       </div>
--- src/pages/FlashcardsPage.tsx
+++ src/pages/FlashcardsPage.tsx
@@ -3,7 +3,7 @@ import Flashcard from "../components/Flashcard";
 const FlashcardsPage: React.FC = () => {
   return (
     <div className="max-w-5xl m-auto">
-      <div className="text-2xl font-bold mb-4">Flashcards</div>
+      <div className="text-2xl font-bold mb-4">My Flashcards</div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <Flashcard />
         <Flashcard />
